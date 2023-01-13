fe766565a83739441e065bbc783c9401f7eeca1d
leanna2312 <tojuny1223@gmail.com>
2023-01-13 23:31:01 +0900

    naming lowerCase again

---
+++ demo/src/main/java/com/example/demo/dto/OrderNumberDto.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderNumberDto {
+
+    Long orderId;
+    Long oderNumber;
+
+    Long Order(Long orderId){
+        return this.oderNumber;
+    }
+
+}
