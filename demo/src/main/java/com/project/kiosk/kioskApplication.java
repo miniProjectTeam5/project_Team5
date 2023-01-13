ac4256572b2913e14d82fb8c17c043df2f8705d6
lcoeda <120078825+lcoeda@users.noreply.github.com>
2023-01-13 16:21:47 +0900

    초기 설정

---
+++ demo/src/main/java/com/project/kiosk/kioskApplication.java
@@ -0,0 +1,13 @@
+package com.project.kiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class kioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(kioskApplication.class, args);
+    }
+
+}
