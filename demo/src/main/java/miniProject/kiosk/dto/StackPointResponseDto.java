95eab1819f82334d0979ffa7483a5e4ad4635c6e
Hyeunseung <hyeunseung03@gmail.com>
2023-01-20 18:03:04 +0900

    포인트 재수정

--- demo/src/main/java/miniProject/kiosk/dto/StackPointResponseDto.java
+++ demo/src/main/java/miniProject/kiosk/dto/StackPointResponseDto.java
@@ -1,7 +1,9 @@
 package miniProject.kiosk.dto;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.Getter;
 
+@Getter
 public class StackPointResponseDto {
     private Integer points;
 
